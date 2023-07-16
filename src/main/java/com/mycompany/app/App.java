fb0e97ad82a54c5ebb502f9010f22908e76fafcc
mishbahulm <munir.m3@gmail.com>
2023-07-16 13:02:44 +0700

    edit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
 /**
  * Hello world!
  */
@@ -15,47 +13,24 @@ public class App {
     public static final String AM = "good morning";
     public static final String PM = "good afternoon";
     public static final String EVE = "good evening";
-    private static final String MESSAGE = "Hello ";
+    private static final String MESSAGE = "Hello, ";
 
     public App() {}
 
     public static void main(String[] args) {
 
-        System.out.println(MESSAGE);
+        Scanner keyboard = new Scanner(System.in);
 
-        // Scanner keyboard = new Scanner(System.in);
-
-        // int computerNumber = (int) (Math.random()*100 + 1);
-        // int userAnswer = 0;
-        // // System.out.println("The correct guess would be " + computerNumber);
-        // int count = 1;
-
-        // System.out.print("Your Name: ");
-        // String name = keyboard.nextLine();
-
-        // Calendar time = Calendar.getInstance();
-        // System.out.println(MESSAGE + name + ", " + decodePeriod(time) + "!");
-        // System.out.println("Try to guess the number for less than a minute.");
-
-        // while (userAnswer != computerNumber)
-        // {
-        //     System.out.println("========= Guessing Game =========");
-        //     System.out.print("Enter a guess (between 1 and 100): ");
-
-        //     try {
-        //         userAnswer = keyboard.nextInt();
-
-        //         System.out.println(determineGuess(userAnswer, computerNumber, count, name));
-
-        //         count++;
-                
-        //     } catch (Exception e) {
-        //         System.out.println("Wrong format!\n" + e);
-        //         break;
-        //     }
+        System.out.print("Your Name: ");
+        String name = keyboard.nextLine();
             
-        // }  
-
+            Calendar time = Calendar.getInstance();
+            System.out.println(MESSAGE + "there , " + decodePeriod(time) + "!");
+            
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+            LocalDateTime now = LocalDateTime.now();  
+            System.out.println("Now:" + dtf.format(now));
+        
     }
 
     public String getMessage() {
@@ -82,25 +57,5 @@ public class App {
             decodedPeriod = AM;
         }
         return decodedPeriod;
-    }
-
-
-    // source: https://hackr.io/blog/java-projects
-    public static String determineGuess(int userAnswer, int computerNumber, int count, String name){
-        if (userAnswer <=0 || userAnswer >100) {
-            return "Your guess is invalid";
-        }
-        else if (userAnswer == computerNumber ){
-            return "Correct! " + name + ", you awesome. \nTotal Guesses: " + count;
-        }
-        else if (userAnswer > computerNumber) {
-            return "Your guess is too high, try again.\nTry Number: " + count;
-        }
-        else if (userAnswer < computerNumber) {
-            return "Your guess is too low, try again.\nTry Number: " + count;
-        }
-        else {
-            return "Your guess is incorrect\nTry Number: " + count;
-        }
     }
 }
