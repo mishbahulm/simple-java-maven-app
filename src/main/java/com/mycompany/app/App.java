70bacfc496470d57efe548df826f4b85ff556589
mishbahulm <munir.m3@gmail.com>
2023-07-16 14:26:53 +0700

    adding system info in app

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,11 @@
 package com.mycompany.app;
 
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-import java.util.Scanner;
 
 /**
  * Hello world!
@@ -18,19 +20,63 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now();
 
-        Scanner keyboard = new Scanner(System.in);
+        System.out.println("=========================");
+        System.out.println("==     System info     ==");
+        System.out.println("== " + dtf.format(now)+" ==");
+        System.out.println("=========================");
 
-        System.out.print("Your Name: ");
-        String name = keyboard.nextLine();
-            
-            Calendar time = Calendar.getInstance();
-            System.out.println(MESSAGE + "there, " + decodePeriod(time) + "!");
-            
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-            LocalDateTime now = LocalDateTime.now();  
-            System.out.println("Now:" + dtf.format(now));
+        Calendar time = Calendar.getInstance();
+        String userName = System.getProperty("user.name");
+        System.out.println(MESSAGE + userName +", " + decodePeriod(time) + "! This yours system info..");
+        System.out.println();
         
+          
+        
+        System.out.println("Hostname: " + System.getenv().get("HOSTNAME"));
+        System.out.println("System property: " + System.getProperty("user.dir"));
+        System.out.println("Operating System: " + System.getProperty("os.name"));
+        System.out.println("Java runtime version: " + System.getProperty("java.runtime.version" ));
+        System.out.println();
+
+        long memorySize = ((com.sun.management.OperatingSystemMXBean) ManagementFactory
+            .getOperatingSystemMXBean()).getTotalPhysicalMemorySize();
+        System.out.println(String.format("Memory Size: %.2f GB", 
+            (double)memorySize/1073741824) +" GB");
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        System.out.println(String.format("Initial memory: %.2f GB", 
+            (double)memoryMXBean.getHeapMemoryUsage().getInit() /1073741824));
+        System.out.println(String.format("Used heap memory: %.2f GB",
+            (double)memoryMXBean.getHeapMemoryUsage().getUsed() /1073741824));
+        System.out.println(String.format("Max heap memory: %.2f GB", 
+            (double)memoryMXBean.getHeapMemoryUsage().getMax() /1073741824));
+        System.out.println(String.format("Committed memory: %.2f GB", 
+            (double)memoryMXBean.getHeapMemoryUsage().getCommitted() /1073741824));      
+        Runtime rs = Runtime.getRuntime();
+        System.out.println("Free memory in JVM before Garbage Collection = "+
+            String.format("Memory Size: %.2f GB", 
+            (double)(rs.freeMemory())/1073741824) +" GB");
+        rs.gc();
+        System.out.println("Free memory in JVM after Garbage Collection = "+
+            String.format("Memory Size: %.2f GB", 
+            (double)(rs.freeMemory())/1073741824) +" GB");
+        System.out.println();
+
+        // System.out.println("Total space: " + 
+        //     (double)new File("/").getTotalSpace()/1024/1024/1024 + " GB");
+        System.out.println(String.format("Total space: %.2f GB", 
+            (double)new File("/").getTotalSpace() /1073741824));
+        System.out.println(String.format("Free space: %.2f GB", 
+            (double)new File("/").getFreeSpace() /1073741824));
+        System.out.println(String.format("Usable space: %.2f GB", 
+            (double)new File("/").getUsableSpace() /1073741824));
+        // System.out.println("More:");
+        // System.getenv().forEach((k, v) -> {
+        //     System.out.println(k + ":" + v);
+        // });
+        System.out.println("=========================");
     }
 
     public String getMessage() {
